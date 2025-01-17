40ab217c2f5d1cef233557841179ccd4e8af5509
Kremu <90266455+Kr3mu@users.noreply.github.com>
2025-01-17 19:06:32 +0100

    fix manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,14 +6,12 @@ author "kr3mu"
 description "script for car selling"
 version "1.0"
 
-client_scripts {
-    "client.lua",
-    "config.lua"
-}
+client_script "client.lua"
 server_script "server.lua"
 
 
 shared_scripts {
     "@es_extended/imports.lua",
     "@ox_lib/init.lua",
+    "config.lua"
 }
